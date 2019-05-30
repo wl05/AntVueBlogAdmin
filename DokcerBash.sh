cf2e567b03300dc42d57cf8da30492827766630c
wangle <2929712050@qq.com>
2019-05-30 19:58:40 +0800

    .

--- DokcerBash.sh
+++ DokcerBash.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-docker stop ant-vue-blog-front
-docker rm ant-vue-blog-front
-docker rmi ant-vue-blog-front
-docker build -t ant-vue-blog-front .
-docker run \
--p 3000:80 \
--d --name ant-vue-blog-front \
-ant-vue-blog-front
-=======
 docker stop ant-vue-blog-admin
 docker rm ant-vue-blog-admin
 docker rmi ant-vue-blog-admin
@@ -17,4 +7,4 @@ docker run \
 -p 3000:80 \
 -d --name ant-vue-blog-admin \
 ant-vue-blog-admin
->>>>>>> 6fc6a46b2db576a3f11175697362b56f063c619d
+
